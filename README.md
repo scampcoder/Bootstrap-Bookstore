ffc6943d1dca18483560a59a64daab65f031cb4f
Victoria Kee <victoriakee95@gmail.com>
2021-10-04 14:59:59 -0500

    Gallery styles and anim

---
+++ README.md
@@ -0,0 +1,5 @@
+# What I Learned:
+
+- How to use lightbox2 to create a more stylized and sleek looking gallery
+
+-
--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+$(document).ready(function() {
+  //Lightbox Options
+    lightbox.option({
+      'wrapAround': true
+    })
+    //Gallery anim
+    $(window).scroll(function() {
+      let position = $(this).scrollTop();
+      if(position >= 350){
+        $('.gallery').addClass('change');
+      } else {
+        $('.gallery').removeClass('change');
+      }
+    })
+})
+
+
 
 
 
